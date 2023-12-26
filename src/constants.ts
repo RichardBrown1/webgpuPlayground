47efc3464af996b8117f3ce67b2ad2078ec3c838
RichardBrown1 <richardbrown1999@outlook.com>
2023-12-26 23:51:33 +1000

    adding mandlebrot files wip

--- src/constants.ts
+++ src/constants.ts
@@ -3,12 +3,12 @@ export const CONSTANTS = {
         COLOURED_SQUARE: "coloured_square",
         PSP_WAVES: "psp_waves",
         CUBE: "cube",
-        DES: "des"
+        MANDLEBROT: "mandlebrot"
     },
     HEADERS: {
         COLOURED_SQUARE: "Coloured Square",
         PSP_WAVES: "PSP Home Screen Waves",
         CUBE: "Cube",
-        DES: "DES Algorithm"
+        MANDLEBROT: "Mandlebrot"
     }
 }
---
+++ src/models/square.ts
@@ -0,0 +1,10 @@
+export const SquareVerteces = () =>{
+    return new Float32Array([
+        -0.8, -0.8,  // vertex a
+        0.8, -0.8,  // vertex b
+        -0.8,  0.8,  // vertex d
+        -0.8,  0.8,  // vertex d
+        0.8, -0.8,  // vertex b
+        0.8,  0.8,  // vertex c
+    ]);
+}
